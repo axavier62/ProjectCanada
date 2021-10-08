2e515294ba4b4d775d5fb87e764b75a29dc34417
axavier62 <xavierprince777@gmail.com>
2021-10-08 14:27:05 +0530

    Unfold files

--- src/org/task/LibGlobal.java
+++ src/org/task/LibGlobal.java
@@ -1,5 +1,10 @@
 package org.task;
 
 public class LibGlobal {
-
+	public static void checkInDates() {
+		System.out.println("CheckInDates");
+	}
+	public static void main(String[] args) {
+		checkInDates();
+	}
 }
