84b2fc731807fed4bf460f32abd0e9c88e484e38
axavier62 <xavierprince777@gmail.com>
2021-10-08 12:27:35 +0530

    Check out code

--- src/org/task/LibGlobal.java
+++ src/org/task/LibGlobal.java
@@ -1,5 +1,10 @@
 package org.task;
 
 public class LibGlobal {
-
+	public static void checkOutDate() {
+		System.out.println("CheckOutDate");
+	}
+	public static void main(String[] args) {
+		checkOutDate();
+	}
 }
