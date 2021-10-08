db2a26e05eb7b3df5771e174a22967112df5ea26
axavier62 <xavierprince777@gmail.com>
2021-10-08 12:23:57 +0530

    Check In Functionality

--- src/org/task/LibGlobal.java
+++ src/org/task/LibGlobal.java
@@ -1,5 +1,10 @@
 package org.task;
 
 public class LibGlobal {
-
+	public static void checkInDate() {
+		System.out.println("CheckInDate");
+	}
+	public static void main(String[] args) {
+		checkInDate();
+	}
 }
